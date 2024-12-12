278dccb2983b0e4d9ce579f13a4b44988fdd41db
Adrian-Aguilera <cruz74270@gmail.com>
2024-12-11 22:00:43 -0600

    feat: index home page

--- portafolio_Config/urls.py
+++ portafolio_Config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from .views import index
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
    public=True,
 )
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('Portafolio_App.urls')),
     path('tokens/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- portafolio_Config/views.py
+++ portafolio_Config/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Portafolio-Backend index.")
+    return render(request, 'index.html')
