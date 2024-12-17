3a11503244434a335551988bb824b734b47a4ef7
Adrian-Aguilera <cruz74270@gmail.com>
2024-12-17 15:15:57 -0600

    fix: build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,11 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations Portafolio_App
+python manage.py migrate Portafolio_App
+
 # Apply any outstanding database migrations
 python manage.py migrate
+
 
 python manage.py createsuperuser_Custom
